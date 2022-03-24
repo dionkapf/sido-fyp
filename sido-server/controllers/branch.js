aa8f1971dc4f8a951838db0542dd90eb1c52cbb2
Dion Kapfumvuti <dionkapf@gmail.com>
2022-03-24 16:41:25 +0300

    Add branch controller

---
+++ sido-server/controllers/branch.js
@@ -0,0 +1,42 @@
+const Model = require("../models/model").Model;
+
+const generateDateColumn = (date, column_name = "") => {
+  return ` TO_CHAR(${date} :: DATE, 'yyyy') AS ${column_name}`;
+};
+
+const getBranch = (req, res) => {
+  new Model("branch")
+    .select(
+      `branch.id AS id, branch.name AS name, region.name AS region`,
+      "",
+      [parseInt(req.params.id)],
+      ["INNER JOIN region ON region.id = branch.region AND branch.id = $1"]
+    )
+    .then((branch_data) => {
+      res.status(200).json({ success: true, data: branch_data.rows });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const getBranches = (req, res) => {
+  new Model("branch")
+    .select(
+      `branch.id AS id, branch.name AS name, region.name AS region`,
+      "",
+      [],
+      ["INNER JOIN region ON region.id = branch.region"]
+    )
+    .then((branch_data) => {
+      res.status(200).json({ success: true, data: branch_data.rows });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+module.exports = {
+  getBranch,
+  getBranches,
+};
