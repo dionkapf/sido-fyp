92ea89f5bbc3740b599dfba4bce4dde493f53192
Dion Kapfumvuti <dionkapf@gmail.com>
2022-03-24 16:50:42 +0300

    Change data sent in index page

--- sido-server/routes/index.js
+++ sido-server/routes/index.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var router = express.Router();
+const { getDistrict, getDistricts } = require("../controllers/district");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.status(200).json({ message: `Hello USER` });
+router.get("/", (req, res) => {
+  return res.status(200).json({ success: true, message: "Hello World" });
 });
 
 module.exports = router;
