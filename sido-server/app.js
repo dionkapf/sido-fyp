836b5526a9486c6f86c610a612dd71e8bf3328b4
Dion Kapfumvuti <dionkapf@gmail.com>
2022-03-24 16:33:06 +0300

    Add district and region routes to app

--- sido-server/app.js
+++ sido-server/app.js
@@ -1,11 +1,15 @@
+// Module variables
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
+// Routes
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var districtRouter = require("./routes/district");
+var regionRouter = require("./routes/region");
 
 var app = express();
 
@@ -17,6 +21,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/api/district", districtRouter);
+app.use("/api/region", regionRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -30,7 +36,7 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  res.status(err.status || 500).json({ message: "Error" });
+  res.status(err.status || 500).json({ success: false, message: err.message });
 });
 
 module.exports = app;
