fe00f737752832a9ae7e36986880cf77f7b43f6a
Dion Kapfumvuti <dionkapf@gmail.com>
2022-04-06 18:14:07 +0300

    Remove search from sidebar

--- sido-client/components/sidebar.js
+++ sido-client/components/sidebar.js
@@ -4,7 +4,6 @@ import styles from "./sidebar.module.css";
 export default function Sidebar() {
   return (
     <nav className={styles.nav}>
-      <input className={styles.input} placeholder="Search..." />
       <Link href="/">
         <a>Home</a>
       </Link>
