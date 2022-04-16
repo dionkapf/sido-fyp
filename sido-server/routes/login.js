b25851c0c499249d1b1f237237018a7346cf599f
Dion Kapfumvuti <dionkapf@gmail.com>
2022-04-16 20:30:54 +0300

    Add more login routes

--- sido-server/routes/login.js
+++ sido-server/routes/login.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { loginUser, authenticateToken } = require("../middleware/auth");
+const {
+  loginUser,
+  authenticateToken,
+  refreshAccessToken,
+} = require("../middleware/auth");
 const jwt = require("jsonwebtoken");
+
+router.route("/ip").get((req, res) => {
+  const remoteAddress = req.socket.remoteAddress;
+  res.status(200).json({ ip: req.ip, remoteAddress });
+});
 
 router.route("/").get(authenticateToken, (req, res, next) => {
   console.log("I login now!", req.body);
   res.status(200).json({ success: true, data: req.user });
 });
+
+router.route("/token").get(refreshAccessToken);
 
 router.route("/").post(loginUser);
 
