bcd289572670faddf4cf6297f7b8d70a85aa1416
Dion Kapfumvuti <dionkapf@gmail.com>
2022-04-17 01:01:08 +0300

    Remove ip route

--- sido-server/routes/login.js
+++ sido-server/routes/login.js
@@ -7,18 +7,14 @@ const {
 } = require("../middleware/auth");
 const jwt = require("jsonwebtoken");
 
-router.route("/ip").get((req, res) => {
-  const remoteAddress = req.socket.remoteAddress;
-  res.status(200).json({ ip: req.ip, remoteAddress });
-});
-
 router.route("/").get(authenticateToken, (req, res, next) => {
   console.log("I login now!", req.body);
   res.status(200).json({ success: true, data: req.user });
 });
 
-router.route("/token").get(refreshAccessToken);
+router.route("/refresh").get(refreshAccessToken);
 
 router.route("/").post(loginUser);
 
 module.exports = router;
+e;
