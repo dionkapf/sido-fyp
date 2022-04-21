f215a5a86af1d078f9e71d1ecc3061c587b5c994
Dion Kapfumvuti <dionkapf@gmail.com>
2022-04-21 14:38:17 +0300

    Create generic dashboard layout

---
+++ sido-client/components/dashboard-layout.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import styles from "./layout.module.scss";
+import Sidebar from "./sidebar";
+import MenuAppBar from "./menuappbar";
+import Footer from "./footer";
+
+export default function DashboardLayout({ children, title }) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <MenuAppBar />
+      <div className={styles.container}>
+        <Sidebar className={styles.sidebar} />
+        <main className={styles.main}>{children}</main>
+      </div>
+      <Footer />
+    </>
+  );
+}
