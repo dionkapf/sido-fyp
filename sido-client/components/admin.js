c4307023606bed0142042050378a29326214a7be
Dion Kapfumvuti <dionkapf@gmail.com>
2022-04-26 16:17:30 +0300

    Redesign admin component

--- sido-client/components/admin.js
+++ sido-client/components/admin.js
@@ -9,6 +9,19 @@ import Paper from "@material-ui/core/Paper";
 import DashboardLayout from "../components/dashboard-layout";
 import TextField from "@material-ui/core/TextField";
 import styles from "../styles/Hello.module.scss";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -18,17 +31,9 @@ const StyledTableCell = withStyles((theme) => ({
 }))(TableCell);
 
 export default function Admin({ list, title, description, options }) {
-  const columns = [];
-  const colNames = [];
-  const indices = [...Array(list.length).keys()];
-  console.log(indices);
-  for (let key in list[0]) {
-    colNames.push(key);
-    if (key === "id") key = "ID";
-    let newKey = key[0].toUpperCase() + key.slice(1);
-    newKey = newKey.replace(/_/g, " ");
-    columns.push(newKey);
-  }
+  const columns = Object.keys(list[0]);
+  const classes = useStyles();
+  console.log("columns", columns);
   return (
     <DashboardLayout title={title} sidebarOptions={options}>
       <main className={styles.main}>
@@ -44,29 +49,93 @@ export default function Admin({ list, title, description, options }) {
             <TableHead>
               <TableRow>
                 {columns.map((column) => {
-                  if (column === "ID")
+                  if (column === "S/N")
                     return (
-                      <StyledTableCell align="left">{column}</StyledTableCell>
+                      <StyledTableCell align="left" key={column}>
+                        {column}
+                      </StyledTableCell>
                     );
                   else
                     return (
-                      <StyledTableCell align="right">{column}</StyledTableCell>
+                      <StyledTableCell align="right" key={column}>
+                        {column}
+                      </StyledTableCell>
                     );
                 })}
               </TableRow>
             </TableHead>
             <TableBody>
               {list.map((row) => (
-                <TableRow key={row.id}>
-                  {colNames.map((column) => {
-                    if (column === "id")
-                      return (
-                        <TableCell align="left" component="th" scope="row">
-                          {list.findIndex((item) => item.id === row.id) + 1}
-                        </TableCell>
-                      );
-                    else
-                      return <TableCell align="right">{row[column]}</TableCell>;
+                <TableRow key={row["S/N"]}>
+                  {columns.map((column) => {
+                    switch (column) {
+                      case "S/N":
+                        return (
+                          <TableCell
+                            align="left"
+                            component="th"
+                            scope="row"
+                            key={column}
+                          >
+                            {row[column]}
+                          </TableCell>
+                        );
+                      case "Phone":
+                        const raw_phone = row[column];
+                        const phone_number =
+                          raw_phone[0] == "0" ? (
+                            <a href={`tel:+255${raw_phone.slice(1)}`}>
+                              {"+255 " +
+                                raw_phone
+                                  .slice(1)
+                                  .match(/.{1,3}/g)
+                                  .join(" ")}
+                            </a>
+                          ) : (
+                            <a href={`tel:${raw_phone}`}>
+                              {raw_phone.match(/.{1,3}/g).join(" ")}
+                            </a>
+                          );
+
+                        return (
+                          <TableCell align="right" key={column}>
+                            {phone_number}
+                          </TableCell>
+                        );
+                      case "Email":
+                        return (
+                          <TableCell align="right" key={column}>
+                            <a href={`mailto:${row[column]}`}>{row[column]}</a>
+                          </TableCell>
+                        );
+                      case "Actions":
+                        const actions = row[column];
+                        return (
+                          <TableCell
+                            align="right"
+                            className={classes.root}
+                            key={column}
+                          >
+                            <ButtonGroup
+                              variant="text"
+                              color="primary"
+                              aria-label="text primary button group"
+                            >
+                              {actions.map((action) => (
+                                <Button key={action.name} href={action.url}>
+                                  {action.name}
+                                </Button>
+                              ))}
+                            </ButtonGroup>
+                          </TableCell>
+                        );
+                      default:
+                        return (
+                          <TableCell align="right" key={column}>
+                            {row[column]}
+                          </TableCell>
+                        );
+                    }
                   })}
                 </TableRow>
               ))}
