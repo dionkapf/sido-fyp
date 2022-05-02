acc961b051a3ad1c5acfe7f462ba930caedb700d
Dion Kapfumvuti <dionkapf@gmail.com>
2022-05-02 20:33:37 +0300

    Add executive and operations routes

--- sido-server/routes/staff.js
+++ sido-server/routes/staff.js
@@ -1,8 +1,11 @@
 let express = require("express");
 let router = express.Router();
 const { getStaff, getStaffs } = require("../controllers/staff");
+const { getExecutives, getOperators } = require("../controllers/users");
 
 router.route("/").get(getStaffs);
+router.route("/executives").get(getExecutives);
+router.route("/operators").get(getOperators);
 router.route("/:id").get(getStaff);
 
 module.exports = router;
