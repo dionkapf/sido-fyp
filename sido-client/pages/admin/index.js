27baeb06d87a35f6b51a55c2ea268e2b2d08b395
Dion Kapfumvuti <dionkapf@gmail.com>
2022-05-02 20:28:15 +0300

    Redirect to login if not admin and improve fetch performance

--- sido-client/pages/admin/index.js
+++ sido-client/pages/admin/index.js
@@ -1,5 +1,8 @@
 import DashboardLayout from "../../components/dashboard-layout";
 import AdminDashboard from "../../components/admin-dashboard";
+import { useAuth } from "../../context/AuthContext";
+import Router, { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export const adminOptions = [
   {
@@ -39,26 +42,42 @@ export default function Admin({ list }) {
       href: "/admin/users",
     },
   ];
+
+  const { user, setUser } = useAuth();
+  const router = useRouter();
+  useEffect(() => {
+    console.log("Admin user", user);
+    if (!user) {
+      router.push("/login");
+    } else if (user.role !== 1) {
+      router.push("/");
+    }
+  }, [router, user]);
+
   return (
-    <DashboardLayout sidebarOptions={adminOptions}>
+    <DashboardLayout sidebarOptions={adminOptions} title="Admin Dashboard">
       <AdminDashboard cardData={list} />
     </DashboardLayout>
   );
 }
 
 export async function getServerSideProps() {
-  const userCountRes = await (
-    await fetch(`http://localhost:5000/api/owners?count`)
-  ).json();
-  const execCountRes = await (
-    await fetch(`http://localhost:5000/api/staff/executives?count`)
-  ).json();
-  const opsCountRes = await (
-    await fetch(`http://localhost:5000/api/staff/operators?count`)
-  ).json();
-  const userCount = userCountRes.data;
-  const execCount = execCountRes.data;
-  const opsCount = opsCountRes.data;
+  const [userCountRes, executiveCountRes, operationCountRes] =
+    await Promise.all([
+      fetch("http://localhost:5000/api/owners?count"),
+      fetch("http://localhost:5000/api/staff/executives?count"),
+      fetch("http://localhost:5000/api/staff/operators?count"),
+    ]);
+  const [userCountJSON, executiveCountJSON, operationCountJSON] =
+    await Promise.all([
+      userCountRes.json(),
+      executiveCountRes.json(),
+      operationCountRes.json(),
+    ]);
+
+  const userCount = userCountJSON.data;
+  const execCount = executiveCountJSON.data;
+  const opsCount = operationCountJSON.data;
   const userDesc =
     userCount == 1 ? "registered end user" : "registered end users";
   const execDesc = execCount == 1 ? "executive" : "executives";
