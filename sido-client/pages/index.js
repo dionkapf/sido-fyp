78d98f264477e0c03628a35c64c17ba9b261d094
Dion Kapfumvuti <dionkapf@gmail.com>
2022-05-02 16:21:49 +0300

    Tweak home page

--- sido-client/pages/index.js
+++ sido-client/pages/index.js
@@ -1,10 +1,21 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Footer from "../components/footer";
+import { useAuth } from "../context/AuthContext";
+import { useEffect, useRef } from "react";
+import MenuAppBar from "../components/menuappbar";
 
 export default function Home() {
+  const { user, setUser, isLoading, setIsLoading } = useAuth();
+  const title = useRef("Welcome to the SIDO APP");
+  const name = user
+    ? `Welcome back, ${user.first_name}`
+    : "Welcome to the SIDO APP";
+  const formalized = true;
+  useEffect(() => {
+    // console.log("Formalized", user.formalized);
+  }, [name, user]);
   return (
     <div className={styles.container}>
       <Head>
@@ -12,27 +23,29 @@ export default function Home() {
         <meta name="description" content="SIDO Loan and Formalise App" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <MenuAppBar />
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to the SIDO APP</h1>
+        <h1 className={styles.title}>{name}</h1>
 
-        <p className={styles.description}>
-          The backend is served on
-          <code className={styles.code}>http://localhost:5000/</code>
-        </p>
+        <p className={styles.description}>What would you like to do today?</p>
         <div className={styles.grid}>
           <div className={styles.card}>
-            <Link href="/layout" className={styles.active} passHref>
+            <Link href="/apply-loan" className={styles.active} passHref>
               <div>
-                <h2>Documentation &rarr;</h2>
-                <p>Find in-depth information about Next.js features and API.</p>
+                <h2>Apply for a loan &rarr;</h2>
+                <p>
+                  Get up to TSh 15,000,000 in start up capital for your business
+                </p>
               </div>
             </Link>
           </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="/form-request" className={styles.card}>
+            <h2>Rasmilishe  &rarr;</h2>
+            <p>
+              Start the process of formalizing your business and earn your
+              rights
+            </p>
           </a>
         </div>
       </main>
