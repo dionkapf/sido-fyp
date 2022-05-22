345b77f1565b091e527bd3fd906312192b2b4687
Dion Kapfumvuti <dionkapf@gmail.com>
2022-05-22 23:29:08 +0300

    Add loan request form

--- sido-client/pages/apply-loan.js
+++ sido-client/pages/apply-loan.js
@@ -1,21 +1,33 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Link from "next/link";
+import { useRouter } from "next/router";
 import Footer from "../components/footer";
 import { useAuth } from "../context/AuthContext";
 import { useEffect, useRef } from "react";
 import MenuAppBar from "../components/menuappbar";
+import LoanRequest from "../components/loan-request";
 
-export default function ApplyLoan() {
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:5000/api/branches`);
+  const data = await res.json();
+  const branches = data.data;
+  return { props: { branches } };
+}
+
+export default function ApplyLoan({ branches }) {
   const { user, setUser, isLoading, setIsLoading } = useAuth();
   const title = useRef("Welcome to the SIDO APP");
+  const router = useRouter();
   const name = user
     ? `Welcome back, ${user.first_name}`
     : "Welcome to the SIDO APP";
   const formalized = true;
   useEffect(() => {
-    // console.log("Formalized", user.formalized);
-  }, [name, user]);
+    if (!user) {
+      console.log("User is not logged in");
+      router.push("/login");
+    }
+  }, [user, router]);
   return (
     <div className={styles.container}>
       <Head>
@@ -28,9 +40,7 @@ export default function ApplyLoan() {
         <h1 className={styles.title}>{name}</h1>
 
         <p className={styles.description}>What would you like to do today?</p>
-        <h1>
-          LOAN TIME
-        </h1>
+        <LoanRequest branches={branches} />
       </main>
 
       <Footer />
