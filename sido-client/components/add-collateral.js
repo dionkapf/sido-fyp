b0d0994c499d39805ffcfd5a30d9149f3482becf
Dion Kapfumvuti <dionkapf@gmail.com>
2022-05-23 00:38:19 +0300

    Register collateral page

---
+++ sido-client/components/add-collateral.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { useFormik } from "formik";
+import { useAuth } from "../context/AuthContext";
+import { useRouter } from "next/router";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object({
+  worth: Yup.number()
+    .required("Required")
+    .positive("Must be a positive number")
+    .integer("Must be an integer")
+    .min(50000, "Must be worth at least TSh 50 000"),
+});
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "10px",
+    "& > *": {
+      margin: theme.spacing(1),
+      minWidth: "100%",
+    },
+  },
+
+  form: {
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function AddCollateral() {
+  const classes = useStyles();
+  const router = useRouter();
+  const { user, setUser, isLoading, setIsLoading } = useAuth();
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      type: "",
+      worth: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      // alert("Success");
+      const loanRequest = JSON.parse(localStorage.getItem("loanRequest"));
+      loanRequest.collateral = values;
+      localStorage.setItem("loanRequest", JSON.stringify(loanRequest));
+      router.push("/loan-request-success");
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit} className={classes.paper}>
+      <TextField
+        required
+        autoFocus
+        id="name"
+        name="name"
+        label="Item Name"
+        variant="outlined"
+        onChange={formik.handleChange}
+        defaultValue={formik.values.name}
+      />
+      <TextField
+        required
+        id="type"
+        name="type"
+        label="Description"
+        variant="outlined"
+        onChange={formik.handleChange}
+        defaultValue={formik.values.type}
+      />
+      <TextField
+        required
+        id="worth"
+        name="worth"
+        label="Worth"
+        variant="outlined"
+        onChange={formik.handleChange}
+        value={formik.values.worth}
+        error={formik.touched.worth && Boolean(formik.errors.worth)}
+        helperText={formik.touched.worth && formik.errors.worth}
+        InputProps={{
+          startAdornment: <InputAdornment position="start">TSh</InputAdornment>,
+        }}
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        color="primary"
+        className={classes.submit}
+      >
+        ADD COLLATERAL
+      </Button>
+    </form>
+  );
+}
---
+++ sido-client/pages/register-collateral.js
@@ -0,0 +1,60 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import Footer from "../components/footer";
+import { useAuth } from "../context/AuthContext";
+import { useEffect, useRef } from "react";
+import MenuAppBar from "../components/menuappbar";
+import AddCollateral from "../components/add-collateral";
+import { makeStyles } from "@material-ui/core";
+
+export async function getServerSideProps() {
+  const res = await fetch(`http://localhost:5000/api/branches`);
+  const data = await res.json();
+  const branches = data.data;
+  return { props: { branches } };
+}
+
+const useStyles = makeStyles((theme) => ({
+  main: {
+    minHeight: "75vh",
+    padding: "2rem 0",
+    flex: "1",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    padding: "0 0rem",
+  },
+}));
+
+export default function RegisterCollateral({ branches }) {
+  const { user, setUser, isLoading, setIsLoading } = useAuth();
+  const title = useRef("Welcome to the SIDO APP");
+  const router = useRouter();
+  const classes = useStyles();
+  useEffect(() => {
+    if (!user) {
+      console.log("User is not logged in");
+      router.push("/login");
+    }
+  }, [user, router]);
+  return (
+    <div className={classes.container}>
+      <Head>
+        <title>Add collateral for loan</title>
+        <meta name="description" content="SIDO Loan and Formalise App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MenuAppBar />
+      <main className={classes.main}>
+        <h1 className={styles.title}>Register collateral</h1>
+        <AddCollateral />
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
