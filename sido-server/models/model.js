bed4b9b7a5926d9c604b75e4bd5df8c906e2305c
Dion Kapfumvuti <dionkapf@gmail.com>
2022-06-15 16:12:00 +0300

    Add update funciton to model

--- sido-server/models/model.js
+++ sido-server/models/model.js
@@ -27,6 +27,20 @@ class Model {
     return this.pool.query(query);
   }
 
+  async update(columns, values, id) {
+    if (columns.length !== values.length)
+      throw console.error("Values and columns do not match");
+    let query = {
+      text: `UPDATE ${this.table} SET ${this.updateColumns(
+        columns,
+        values
+      )} WHERE id = $1 RETURNING *`,
+      values: [...id, ...values],
+    };
+    console.log(query);
+    return this.pool.query(query);
+  }
+
   async insert(columns, values) {
     if (columns.length !== values.length)
       throw console.error("Values and columns do not match");
@@ -50,10 +64,24 @@ class Model {
     return this.pool.query(query);
   }
 
-  placeHolderTextMaker(values) {
+  placeHolderTextMaker(values, start = 1) {
     let values_string = [];
-    for (let i = 1; i < values.length + 1; i++) values_string.push(`$${i}`);
+    for (let i = 1; i < values.length + 1; i++)
+      values_string.push(`$${i + (start - 1)}`);
     return values_string;
+  }
+
+  updateColumns(columns, values) {
+    let values_string = this.placeHolderTextMaker(values, 2);
+    let clause = "";
+    columns.forEach((column, index) => {
+      if (index === 0) {
+        clause += `${column} = ${values_string[index]}`;
+      } else {
+        clause += `, ${column} = ${values_string[index]}`;
+      }
+    });
+    return clause;
   }
 }
 
